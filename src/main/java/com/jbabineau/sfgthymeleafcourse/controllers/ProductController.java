ee507a2809638039e0a1cef285e135a28d6a9c01
James Babineau <jbabineau@homesite.com>
2018-06-19 12:51:09 -0400

    adding a new url to return objects by id

--- src/main/java/com/jbabineau/sfgthymeleafcourse/controllers/ProductController.java
+++ src/main/java/com/jbabineau/sfgthymeleafcourse/controllers/ProductController.java
@@ -1,12 +1,32 @@
 package com.jbabineau.sfgthymeleafcourse.controllers;
 
+import com.jbabineau.sfgthymeleafcourse.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/product")
     public String getProduct() {
+        return "redirect::/index";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProductById(@RequestParam Integer id, Model model) {
+        model.addAttribute("product", productService.getProduct(id));
+
         return "product";
     }
 }
--- src/main/java/com/jbabineau/sfgthymeleafcourse/services/ProductServiceImpl.java
+++ src/main/java/com/jbabineau/sfgthymeleafcourse/services/ProductServiceImpl.java
@@ -3,6 +3,7 @@ package com.jbabineau.sfgthymeleafcourse.services;
 import com.jbabineau.sfgthymeleafcourse.domain.Author;
 import com.jbabineau.sfgthymeleafcourse.domain.Product;
 import com.jbabineau.sfgthymeleafcourse.domain.ProductCategory;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Service
 public class ProductServiceImpl implements ProductService{
 
     private Map<Integer, Product> productMap;
